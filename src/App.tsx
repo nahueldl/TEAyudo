8767942bbb42eb1c73a8bc1bf346ecc74ce00432
LPapazian <lilapapazian@gmail.com>
2020-06-29 23:27:50 -0300

    modify imports because of changes in the folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import Menu from './components/Menu';
-import React from 'react';
-import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router-dom';
+import Menu from "./components/Menu";
+import React from "react";
+import { IonApp, IonRouterOutlet, IonSplitPane } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { Redirect, Route } from "react-router-dom";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
-import Inicio from './Inicio/Inicio';
-import Pictogramas from './Pictogramas/Pictogramas';
-import Categorias from './Categorias/Categorias';
-import Profesionales from './Profesionales/Profesionales';
-import Informes from './Informes/Informes';
-import Pacientes from './Pacientes/Pacientes';
-import Configuracion from './Configuracion/Configuracion';
+import "./theme/variables.css";
+import Inicio from "./Vistas/Inicio/Inicio";
+import Pictogramas from "./Vistas/Pictogramas/Pictogramas";
+import Categorias from "./Vistas/Categorias/Categorias";
+import Profesionales from "./Vistas/Profesionales/Profesionales";
+import Informes from "./Vistas/Informes/Informes";
+import Pacientes from "./Vistas/Pacientes/Pacientes";
+import Configuracion from "./Vistas/Configuracion/Configuracion";
 
 const App: React.FC = () => {
-//Agregar el auth+redirect a un home fuera de la app
+  //Agregar el auth+redirect a un home fuera de la app
   return (
     <IonApp>
       <IonReactRouter>
@@ -47,6 +47,7 @@ const App: React.FC = () => {
             <Route path="/page/informes" component={Informes} exact />
             <Route path="/page/configuracion" component={Configuracion} exact />
             <Redirect from="/" to="/page/inicio" exact />
+            {/*Agregar redirect obligatorio para elección de paciente inicial*/}
           </IonRouterOutlet>
         </IonSplitPane>
       </IonReactRouter>
