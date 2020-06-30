f532a8f3df8e2c3537f6d85edbf40af7a3ad54fa
LPapazian <lilapapazian@gmail.com>
2020-06-29 23:27:18 -0300

    add title to menu, remove unnecesary components

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,15 +3,13 @@ import {
   IonItem,
   IonLabel,
   IonList,
-  IonListHeader,
   IonMenu,
   IonMenuToggle,
-  IonNote,
-} from '@ionic/react';
+} from "@ionic/react";
 
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import './Menu.css';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import "./Menu.css";
 
 interface AppPage {
   url: string;
@@ -22,29 +20,29 @@ interface AppPage {
 
 const appPages: AppPage[] = [
   {
-    title: 'Pictogramas',
-    url: '/page/pictogramas',
+    title: "Pictogramas",
+    url: "/page/pictogramas",
   },
   {
-    title: 'Categorías',
-    url: '/page/categorias',
+    title: "Categorías",
+    url: "/page/categorias",
   },
   {
-    title: 'Pacientes',
-    url: '/page/pacientes',
+    title: "Pacientes",
+    url: "/page/pacientes",
   },
   {
-    title: 'Profesionales',
-    url: '/page/profesionales',
+    title: "Profesionales",
+    url: "/page/profesionales",
   },
   {
-    title: 'Informes',
-    url: '/page/informes',
+    title: "Informes",
+    url: "/page/informes",
   },
   {
-    title: 'Configuración',
-    url: '/page/configuracion',
-  }
+    title: "Configuración",
+    url: "/page/configuracion",
+  },
 ];
 
 const Menu: React.FC = () => {
@@ -53,14 +51,21 @@ const Menu: React.FC = () => {
   return (
     <IonMenu contentId="main" type="overlay" swipeGesture>
       <IonContent>
+        <h1>Bienvenidx Nano!</h1>
         <IonList id="menu-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
           {appPages.map((appPage, index) => {
             return (
               <IonMenuToggle key={index} autoHide={false}>
-                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                  <IonLabel>{appPage.title}</IonLabel>
+                <IonItem
+                  className={
+                    location.pathname === appPage.url ? "selected" : ""
+                  }
+                  routerLink={appPage.url}
+                  routerDirection="none"
+                  lines="none"
+                  detail={false}
+                >
+                  <IonLabel className="centered">{appPage.title}</IonLabel>
                 </IonItem>
               </IonMenuToggle>
             );
