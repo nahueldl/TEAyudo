aef101b9ca1e6774ad3e629311cef22475301e8a
lilapapazian <lila.papazian@despegar.com>
2020-07-02 20:02:50 -0300

    add basic structure in Inicio

--- src/Vistas/Inicio/Inicio.tsx
+++ src/Vistas/Inicio/Inicio.tsx
@@ -1,23 +1,42 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
+import React from "react";
 const Inicio: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Inicio</IonTitle>
+        </IonToolbar>
+      </IonHeader>
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButtons slot="start">
-                        <IonMenuButton />
-                    </IonButtons>
-                    <IonTitle>Inicio</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-
-            <IonContent>
-                Cosas del inicio
-            </IonContent>
-        </IonPage>
-    )
-}
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol>Comunicarse</IonCol>
+            <IonCol>Jugar</IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>Pictogramas</IonCol>
+            <IonCol>Categorias</IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default Inicio;
