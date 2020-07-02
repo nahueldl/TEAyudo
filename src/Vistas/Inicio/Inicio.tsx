5f51b58b7c9978e1db2c3f04211924adbb56b463
lilapapazian <lila.papazian@despegar.com>
2020-07-02 20:36:16 -0300

    add grid and flex

--- src/Vistas/Inicio/Inicio.tsx
+++ src/Vistas/Inicio/Inicio.tsx
@@ -9,8 +9,15 @@ import {
   IonGrid,
   IonRow,
   IonCol,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
 } from "@ionic/react";
 import React from "react";
+//import styles from './styles.scss';
+
 const Inicio: React.FC = () => {
   return (
     <IonPage>
@@ -26,13 +33,65 @@ const Inicio: React.FC = () => {
       <IonContent>
         <IonGrid>
           <IonRow>
-            <IonCol>Comunicarse</IonCol>
-            <IonCol>Jugar</IonCol>
+            <IonCol size="12" sizeMd="6">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                  <IonCardTitle>Card Title</IonCardTitle>
+                </IonCardHeader>
+
+                <IonCardContent>
+                  Keep close to Nature's heart... and break clear away, once in
+                  awhile, and climb a mountain or spend a week in the woods.
+                  Wash your spirit clean.
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+            <IonCol size="12" sizeMd="6">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                  <IonCardTitle>Card Title</IonCardTitle>
+                </IonCardHeader>
+
+                <IonCardContent>
+                  Keep close to Nature's heart... and break clear away, once in
+                  awhile, and climb a mountain or spend a week in the woods.
+                  Wash your spirit clean.
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
           </IonRow>
-          <IonRow>
-            <IonCol>Pictogramas</IonCol>
-            <IonCol>Categorias</IonCol>
-          </IonRow>
+          {/* <IonRow>
+            <IonCol size="6" size-sm>
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                  <IonCardTitle>Card Title</IonCardTitle>
+                </IonCardHeader>
+
+                <IonCardContent>
+                  Keep close to Nature's heart... and break clear away, once in
+                  awhile, and climb a mountain or spend a week in the woods.
+                  Wash your spirit clean.
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+            <IonCol size="6" size-sm>
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                  <IonCardTitle>Card Title</IonCardTitle>
+                </IonCardHeader>
+
+                <IonCardContent>
+                  Keep close to Nature's heart... and break clear away, once in
+                  awhile, and climb a mountain or spend a week in the woods.
+                  Wash your spirit clean.
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>*/}
         </IonGrid>
       </IonContent>
     </IonPage>
