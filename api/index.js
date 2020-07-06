39aa91a2af87a96254ef575817a6fb92e0c3a330
Nahuel De Leonardis <deleonardisnahuel@gmail.com>
2020-07-06 20:17:01 -0300

    Added import & check for env_variables

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const requestLogger = require('./middleware/loggers/requestLogger');
-const { builtinModules } = require('module');
+const requestLogger = require('./middleware/requestLogger');
+const { isNullOrUndefined } = require('util');
 
-//Config (TODO:sacarlo de aca)
-const PORT = process.env.PORT || 8080;
+if(isNullOrUndefined(process.env.NODE_ENV)){
+    console.log("Faltan configurar las variables ambientales");
+    process.exit(1);
+}
+const PORT = process.env.PORT;
 
 const app = express();
 
