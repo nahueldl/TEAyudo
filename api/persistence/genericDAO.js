0c699762101a8d55601fed4b9a02216fc5988fb6
Nahuel De Leonardis <deleonardisnahuel@gmail.com>
2020-07-07 13:32:11 -0300

    Add generic runQuery with params

--- api/persistence/genericDAO.js
+++ api/persistence/genericDAO.js
@@ -1,11 +1,15 @@
-const sql = require('mssql')
+const sql = require('mssql');
+const { isNullOrUndefined } = require('util');
 
 const genericDAO = {
-	runQuery: async function (query){
+	runSimpleQuery: async function (query){
+		if(isNullOrUndefined(query)) throw 'query no ha sido definida';
+
 		const res = {
 			state: null,
 			response: null
 		};
+
 		try{
 			await sql.connect(`mssql://${process.env.dbuser}:${process.env.dbpass}@${process.env.dbservername}/${process.env.dbname}?encrypt=true`);
 			const result = await sql.query(query);
@@ -15,9 +19,39 @@ const genericDAO = {
 			res.state = false;
 			res.response = err;
 		}
+
 		sql.close();
 		return res;
-	}
+	},
+	runQuery: async function (query, params){//name type value
+		if(isNullOrUndefined(query)) throw 'query no ha sido definida';
+		if(isNullOrUndefined(params) || params.length < 1) throw 'no se han definido parametros';
+
+		const res = {
+			state: null,
+			response: null
+		};
+
+		try{
+			const pool = await sql.connect(`mssql://${process.env.dbuser}:${process.env.dbpass}@${process.env.dbservername}/${process.env.dbname}?encrypt=true`);
+			const request = pool.request();
+			params.forEach(param => {
+				if(isNullOrUndefined(param.type))
+					request.input(param.name, param.value)
+				else
+					request.input(param.name, param.type, param.value)
+			});
+			const result = await request.query(query);
+			res.state = true;
+			res.response = result.recordsets[0];
+		}catch(err){
+			res.state = false;
+			res.response = err;
+		}
+		
+		sql.close();
+		return res;
+	},
 }
 
 module.exports = genericDAO;
