9e60f0bf36340c91eb903eb02a530f9f18218790
Nahuel De Leonardis <deleonardisnahuel@gmail.com>
2020-07-07 13:31:34 -0300

    Update definition of PORT

--- api/index.js
+++ api/index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require('path');
 const requestLogger = require('./middleware/requestLogger');
 const { isNullOrUndefined } = require('util');
+const sql = require('mssql');
 
 if(isNullOrUndefined(process.env.NODE_ENV)){
     console.log("Faltan configurar las variables ambientales");
     process.exit(1);
-}
-const PORT = process.env.PORT;
+};
 
 const app = express();
 
@@ -21,5 +21,4 @@ app.use('/api/categorias', require('./routes/categoriasController'));
 
 
 
-app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
-
+app.listen(process.env.PORT, () => console.log(`Servidor iniciado en el puerto ${process.env.PORT}`));
