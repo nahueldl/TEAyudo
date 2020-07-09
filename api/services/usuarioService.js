9005f29f6211c032ca55c876ae41d4dee39d24fb
Nahuel De Leonardis <deleonardisnahuel@gmail.com>
2020-07-09 18:53:24 -0300

    Add usuarioService

---
+++ api/services/usuarioService.js
@@ -0,0 +1,27 @@
+const usuarioDAO = require('../persistence/usuarioDAO');
+
+
+const usuarioService = {
+
+	getById: async function(id){
+		//Aca iría la lógia de negocio
+		return await usuarioDAO.getById(parseInt(id));
+	},
+
+
+	insert: async function(usuario){
+        //Aca iría la lógia de negocio
+
+        //TODO: chequear que se carguen los datos del profesional, o del familiar, o de ambos
+        //pero que no falte data
+
+        //Todo validar que el correo cumpla el formato
+
+
+		return await usuarioDAO.insert(usuario);
+	}
+	
+};
+
+
+module.exports = usuarioService;
