ec5dfd7b340cfd968ca68cf8f61be1dae16c0470
Nahuel De Leonardis <deleonardisnahuel@gmail.com>
2020-07-09 18:53:34 -0300

    Add usuarioController

---
+++ api/routes/usuarioController.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const usuarioService = require('../services/usuarioService')
+
+
+//GET Usuario by id
+router.get('/:id', async (req, res) => {
+	const result = await usuarioService.getById(req.params.id)
+
+	if(result.state)
+		res.status(200).json(result.response);
+	else
+		res.status(404).json({msg: `No se encontro usuario con id=${req.params.id}`});    
+});
+
+
+//POST Usuario
+router.post('/register', async (req, res) => {
+	const result = await usuarioService.insert(req.body);
+	if(result.state){
+		res.status(200).json({msg: "El usuario ha sido correctamente creada"});
+	}else{
+		res.status(500).json({msg: "Ha ocurrido un error inesperado en el servidor"});
+	}
+});
+
+
+module.exports = router;
