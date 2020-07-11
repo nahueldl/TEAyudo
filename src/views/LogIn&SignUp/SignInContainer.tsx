224a5f3c8ecc3e930a6d8b6cbfcb31cb1a148b42
LPapazian <lilapapazian@gmail.com>
2020-07-11 10:59:30 -0300

    add mocked validations, navigation in SignInContainer

--- src/views/LogIn&SignUp/SignInContainer.tsx
+++ src/views/LogIn&SignUp/SignInContainer.tsx
@@ -1,75 +1,74 @@
-import React from "react";
-import { IonIcon, IonList, IonItem, IonInput, IonButton } from "@ionic/react";
+import React, { useState, useContext, useCallback } from "react";
+import { IonIcon, IonList, IonItem, IonInput, IonButton, NavContext } from "@ionic/react";
 import { logoGoogle, logoFacebook } from "ionicons/icons";
 import "./styles.css";
 
-class SignInContainer extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+const SignInContainer: React.FC = () => {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [emailValidation, setEmailValidation] = useState<boolean>(false);
+  const [passwordValidation, setPasswordValidation] = useState<boolean>(false);
+  const {navigate} = useContext(NavContext);
+  
+  const validateEmail = () => {
+    email ? setEmailValidation(true) : setEmailValidation(false);
   }
 
-  handleChange(event: any, name: string) {
-    //this.setState({ name: event.detail.value });
+  const validatePassword = () => {
+    password ? setPasswordValidation(true) : setPasswordValidation(false);
   }
 
-  handleSubmit(event: any) {
-    alert("Se hizo submit");
-    event.preventDefault();
+  const validateButton = () => {
+    return !(emailValidation && passwordValidation);
   }
 
-  render() {
-    const { email, password } = this.state;
-    return (
-      <div className="formContainer signInContainer">
-        <form action="#">
-          <h1>Iniciar sesión</h1>
-          <div>
-            <a href="#">
-              <IonIcon icon={logoGoogle} size="large" />
-            </a>
-            <a href="#">
-              <IonIcon icon={logoFacebook} size="large" />
-            </a>
-          </div>
-          <span>O utiliza tu cuenta</span>
-          <IonList>
-            <IonItem>
-              <IonInput
-                name="email"
-                value={email}
-                required
-                clearInput
-                type="email"
-                placeholder="Email"
-                onIonChange={(e) => this.handleChange(e, "email")}
-              ></IonInput>
-            </IonItem>
-            <IonItem>
-              <IonInput
-                name="password"
-                value={password}
-                required
-                clearInput
-                type="password"
-                placeholder="Contraseña"
-                onChange={this.handleChange}
-              ></IonInput>
-            </IonItem>
-          </IonList>
-          <a href="#">¿Olvidaste tu contraseña?</a>
-          <IonButton routerLink="/pacientes">Iniciar sesión</IonButton>
-        </form>
-      </div>
-    );
-  }
-}
+  const handleSubmit = useCallback(() => navigate('/pacientes', 'forward'), [navigate]);
+
+  return (
+    <div className="formContainer signInContainer">
+      <form action="#">
+        <h1>Iniciar sesión</h1>
+        <div>
+          <a href="#">
+            <IonIcon icon={logoGoogle} size="large" />
+          </a>
+          <a href="#">
+            <IonIcon icon={logoFacebook} size="large" />
+          </a>
+        </div>
+        <span>O utiliza tu cuenta</span>
+        <IonList>
+          <IonItem>
+            <IonInput
+              name="email"
+              value={email}
+              required
+              clearInput
+              type="email"
+              placeholder="Email"
+              onIonChange={e => setEmail(e.detail.value!)}
+              onIonBlur={ e => validateEmail()}
+            ></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput
+              name="password"
+              value={password}
+              required
+              clearInput
+              type="password"
+              placeholder="Contraseña"
+              onIonChange={e => setPassword(e.detail.value!)}
+              onIonBlur={e => validatePassword()}
+            ></IonInput>
+          </IonItem>
+        </IonList>
+        <a href="#">¿Olvidaste tu contraseña?</a>
+        <IonButton disabled={validateButton()} onClick={handleSubmit}>Iniciar sesión</IonButton>
+      </form>
+    </div>
+  );
+};
 
 interface Props {}
 
