9316ff4692b72c0439158b7596189f9444faab53
LPapazian <lilapapazian@gmail.com>
2020-07-18 16:51:01 -0300

    handle only axios in authentication service

--- src/services/authentication.services.tsx
+++ src/services/authentication.services.tsx
@@ -1,33 +1,16 @@
 import AxiosWrapper from "../utils/axios";
-import { TEAyudoContext } from "../context";
 
 class AuthenticationServices {
   private axios: AxiosWrapper;
-
-  static contextType = TEAyudoContext;
-  context!: React.ContextType<typeof TEAyudoContext>;
-
   constructor() {
     this.axios = new AxiosWrapper({ useErrorInterceptor: true });
   }
 
   handleLogIn(email: string, password: string) {
-    this.context.setData({ loading: true, error: false });
-    return this.axios
-      .post("/api/usuario/login", { correo: email, password: password })
-      .then((res: any) => {
-        this.context.setData({
-          username: email,
-          authenticated: true,
-          loading: false,
-        });
-      })
-      .catch((error: any) => {
-        this.context.setData({
-          loading: false,
-          error: true,
-        });
-      });
+    return this.axios.post("/api/usuario/login", {
+      correo: email,
+      password: password,
+    });
   }
 
   handleSignUp(
@@ -38,19 +21,13 @@ class AuthenticationServices {
     docNumber?: string,
     licenseNumber?: string
   ) {
-    this.context.setData({ loading: true, error: false });
-
-    return this.axios
-      .post("/api/usuario/register", {
-        nombre: name,
-        apellido: "",
-        correo: email,
-        password: password,
-      })
-      .then((res: any) => {
-        this.context.setData({ loading: false });
-      })
-      .catch((error: any) => this.context.setData({ error: true }));
+    // TODO 
+    return this.axios.post("/api/usuario/register", {
+      nombre: name,
+      apellido: "",
+      correo: email,
+      password: password,
+    });
   }
 }
 
