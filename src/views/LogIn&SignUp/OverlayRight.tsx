8853633b988c31d8b72d1eb2610ac2637a04e452
LPapazian <lilapapazian@gmail.com>
2020-07-27 20:44:29 -0300

    add logo to overlay

--- src/views/LogIn&SignUp/OverlayRight.tsx
+++ src/views/LogIn&SignUp/OverlayRight.tsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { IonButton } from "@ionic/react";
+import { IonButton, IonImg } from "@ionic/react";
 import "./styles.css";
-const OverlayRight: React.FC<Props> = ({handleActivationChange}) => {
+const OverlayRight: React.FC<Props> = ({ handleActivationChange }) => {
   return (
     <div className="overlayPanel overlayRight">
+      <img src="assets/icon/favicon.png" alt="TEAyudo logo" height="200em"/>
       <h1>¿Aún no estás registradx?</h1>
       <p>Para comenzar, sólo necesitas crear una cuenta</p>
-      <IonButton className="overlayButton" fill="outline" onClick={() => handleActivationChange(true)}>Registrarse</IonButton>
+      <IonButton
+        className="overlayButton"
+        fill="outline"
+        onClick={() => handleActivationChange(true)}
+      >
+        Registrarse
+      </IonButton>
     </div>
   );
 };
 
 interface Props {
-  handleActivationChange: (value:boolean) => void;
-};
+  handleActivationChange: (value: boolean) => void;
+}
 
 export default OverlayRight;
