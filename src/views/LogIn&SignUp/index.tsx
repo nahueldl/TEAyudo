eb1e28c957de9627e4de81bbfe6ef4c4fc0054c2
LPapazian <lilapapazian@gmail.com>
2020-07-27 20:44:07 -0300

    change handle sign in and up, modal for rol

--- src/views/LogIn&SignUp/index.tsx
+++ src/views/LogIn&SignUp/index.tsx
@@ -1,6 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useContext, useState, useCallback } from "react";
-import { IonContent, NavContext, IonSlides, IonSlide } from "@ionic/react";
+import {
+  IonContent,
+  NavContext,
+  IonSlides,
+  IonSlide,
+  IonModal,
+  IonButton,
+} from "@ionic/react";
 import { AuthenticationContext } from "../../context/authentication";
 import AuthenticationServices from "../../services/authentication.services";
 import OverlayLeft from "./OverlayLeft";
@@ -12,18 +18,18 @@ import { PlatformContext } from "../../context/platform";
 
 const LogInSignUpPage: React.FC = () => {
   const { navigate } = useContext(NavContext);
-  const { authData, setData } = useContext(AuthenticationContext);
-  const { username } = authData;
+  const { setAuthData } = useContext(AuthenticationContext);
   const { platformData } = useContext(PlatformContext);
   const { isMobile } = platformData;
 
+  const [showModal, setShowModal] = useState<boolean>(true);
   const [showSignIn, setShowSignIn] = useState<boolean>(false);
 
-  const handleSignIn = (email: string, password: string) => {
-    setData({ loading: false, error: true });
+  const handleSignIn = ({ email, password }: any) => {
+    setAuthData({ loading: false, error: true });
     AuthenticationServices.handleLogIn(email, password)
       .then((_res: any) => {
-        setData({
+        setAuthData({
           username: email,
           authenticated: true,
           loading: false,
@@ -31,39 +37,21 @@ const LogInSignUpPage: React.FC = () => {
         goToSelectPatient();
       })
       .catch((_error: any) => {
-        setData({
+        setAuthData({
           loading: false,
           error: true,
         });
       });
   };
 
-  const handleSignUp = (
-    name: string,
-    email: string,
-    password: string,
-    idDoc?: string,
-    docNumber?: string,
-    licenseNumber?: string
-  ) => {
-    setData({ loading: true, error: false });
-    AuthenticationServices.handleSignUp(
-      name,
-      email,
-      password,
-      idDoc,
-      docNumber,
-      licenseNumber
-    )
-      .then((_res: any) => {
-        setData({ loading: false });
-      })
-      .catch((_error: any) => setData({ error: true }));
+  const handleSignUp = ({ name, email, password }: any) => {
+    console.log(name);
+    setAuthData({ loading: true, error: false });
   };
 
   const goToSelectPatient = useCallback(
-    () => navigate(`/${username}/pacientes`, "forward"),
-    [navigate, username]
+    () => navigate("/paciente", "forward"),
+    [navigate]
   );
 
   const classRightPanelActive = showSignIn ? "rightPanelActive" : "";
@@ -76,13 +64,13 @@ const LogInSignUpPage: React.FC = () => {
             <SignInForm signIn={handleSignIn} />
           </IonSlide>
           <IonSlide>
-            <SignUpForm />
+            <SignUpForm signUp={handleSignUp} />
           </IonSlide>
         </IonSlides>
       ) : (
         <div className={`container ${classRightPanelActive}`}>
           <div className="formContainer signUpContainer">
-            <SignUpForm />
+            <SignUpForm signUp={handleSignUp} />
           </div>
           <div className="formContainer signInContainer">
             <SignInForm signIn={handleSignIn} />
@@ -95,6 +83,11 @@ const LogInSignUpPage: React.FC = () => {
           </div>
         </div>
       )}
+      <IonModal isOpen={showModal} backdropDismiss={false}>
+        <p>Para continuar, necesitamos que elijas tu rol</p>
+        {/* TODO rol + explicación de cada uno */}
+        {/* <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton> */}
+      </IonModal>
     </IonContent>
   );
 };
