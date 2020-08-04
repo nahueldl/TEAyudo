5ba9a01890ad349d6610028cabb55ec7d82a390f
LPapazian <lilapapazian@gmail.com>
2020-08-04 20:33:02 -0300

    add modal to select role

--- src/views/LogIn&SignUp/ChooseRoleModal.tsx
+++ src/views/LogIn&SignUp/ChooseRoleModal.tsx
@@ -1,22 +1,110 @@
 import React, { useState } from "react";
-import { IonModal } from "@ionic/react";
+import {
+  IonModal,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonRadioGroup,
+  IonRadio,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonButton,
+} from "@ionic/react";
+import "./styles.css";
 
-const ChooseRoleModal: React.FC<Props> = (chooseRole) => {
-  const [showModal, setShowModal] = useState<boolean>(false);
+type IdType = "DNI" | "PASAPORTE";
+type RolSelected = "F" | "M";
+
+const ChooseRoleModal: React.FC<Props> = ({isOpen, handleSelection}) => {
+  const [rolSelected, setRolSelected] = useState<RolSelected>();
+  const [idType, setIdType] = useState<IdType>();
+  const [idNumber, setIdNumber] = useState<number>();
+  const [licenseNumber, setLicenseNumber] = useState<number>();
+
+  const showSubmitButton = () => {
+    switch (rolSelected) {
+      case "F": return true;
+      case "M": return idType && idNumber && licenseNumber ;
+    }
+  }
+
+  const handleSubmit = () => {
+    handleSelection(idType, idNumber, licenseNumber)
+  }
+
   return (
-    <IonModal isOpen={showModal} backdropDismiss={false}>
-      <p>Para continuar, necesitamos que elijas tu rol</p>
-      {/* TODO rol + explicación de cada uno */}
-      {/* <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton> */}
+    <IonModal isOpen={isOpen} backdropDismiss={false}>
+      <div className="modalBody">
+        <h2 className="title">Para continuar, necesitamos que elijas tu rol</h2>
+        <IonList inset={true}>
+          <IonRadioGroup>
+            <IonItem onClick={() => setRolSelected("F")}>
+              <IonRadio value="familiar" />
+              <IonLabel>
+                <h2 className="itemTitle">Familiar</h2>
+                <h3>
+                  Elegí este rol si sos el familiar de un paciente con TEA
+                </h3>
+              </IonLabel>
+            </IonItem>
+            <IonItem onClick={() => setRolSelected("M")}>
+              <IonRadio value="medico" />
+              <IonLabel>
+                <h2 className="itemTitle">Médicx</h2>
+                <h3>
+                  Elegí este rol si sos le médicx tratante de un paciente con
+                  TEA
+                </h3>
+              </IonLabel>
+            </IonItem>
+          </IonRadioGroup>
+        </IonList>
+        {rolSelected === "M" ? (
+          <div className="doctorData">
+            <h4 className="title">
+              Necesitamos algunos datos extra para verificar
+            </h4>
+            <IonSelect
+              value={idType}
+              placeholder="Selecciona el tipo de documento"
+              onIonChange={(e) => setIdType(e.detail.value)}
+            >
+              <IonSelectOption value="DNI">DNI</IonSelectOption>
+              <IonSelectOption value="PASAPORTE">Pasaporte</IonSelectOption>
+            </IonSelect>
+            <IonItem>
+              <IonInput
+                value={idNumber}
+                placeholder="Número de identificación"
+                onIonChange={(e) => setIdNumber(parseInt(e.detail.value!, 10))}
+                clearInput
+              ></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonInput
+                value={licenseNumber}
+                placeholder="Número de matrícula"
+                onIonChange={(e) =>
+                  setLicenseNumber(parseInt(e.detail.value!, 10))
+                }
+                clearInput
+              ></IonInput>
+            </IonItem>
+          </div>
+        ) : null}
+      </div>
+     <IonButton disabled={!showSubmitButton()} onClick={() => handleSubmit()}>Siguiente</IonButton>
     </IonModal>
   );
 };
 
 interface Props {
-  chooseRole: (
+  isOpen: boolean;
+  handleSelection: (
     idType?: string,
-    idNumber?: string,
-    licenseNumber?: string
+    idNumber?: number,
+    licenseNumber?: number
   ) => void;
 }
 
