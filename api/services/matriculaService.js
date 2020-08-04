f3932b1fbbefe8c2f2c5dbe694a9ae190f2b06a7
Juani Ferro <juanignacioferro8@gmail.com>
2020-08-04 15:47:10 -0300

    very basic, just pushing so i can merge into other branch

--- api/services/matriculaService.js
+++ api/services/matriculaService.js
@@ -2,15 +2,26 @@ const estadosRespuesta = require('../models/estados_respuesta');
 const { isNullOrUndefined } = require('util');
 const axios = require('axios').default;
 
-axios({
-    method: 'get',
-    url: 'https://sisa.msal.gov.ar/sisa/services/rest/profesional/obtener',
-    params: {
-        usuario: "fdgassino",
-        clave: "F3d3R4D0r!"
-    },
-    responseType: 'stream'
-  })
-    .then(function (response) {
-      response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-    });
+const matriculaService = {
+
+    checkMatricula: async function(){
+      axios({
+          method: 'get',
+          url: 'https://sisa.msal.gov.ar/sisa/services/rest/profesional/obtener',
+          params: {
+              usuario: "fdgassino",
+              clave: "F3d3R4D0r!",
+              apellido: "Mazzocchi",
+              nombre: "Sabrina"
+          },
+          responseType: 'stream'
+        })
+        .then((response) => {
+          console.log(response);
+        }, (error) => {
+          console.log(error);
+        });
+    }
+};
+
+module.exports = matriculaService;
