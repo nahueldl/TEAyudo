a9e170c55767fa0fb1de702621988dd64072e4a0
LPapazian <lilapapazian@gmail.com>
2020-08-14 16:17:53 -0300

    add comunications and games pages

---
+++ src/views/Comunication/index.tsx
@@ -0,0 +1,13 @@
+import Page from "../../components/Page";
+import React from "react";
+const ComunicationPage: React.FC = () => {
+  return (
+    <Page pageTitle="Comunicarse" showHomeButton>
+      <div>
+        <div>Configuracion</div>
+      </div>
+    </Page>
+  );
+};
+
+export default ComunicationPage;
