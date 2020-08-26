2d4f0523615906472527ea10b44fcdf00b5fd513
LPapazian <lilapapazian@gmail.com>
2020-08-26 18:25:31 -0300

    working grid version for dnd

--- src/views/Comunication/index.tsx
+++ src/views/Comunication/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Page from "../../components/Page";
 import { ReactSortable } from "react-sortablejs";
 import { IonItem } from "@ionic/react";
-
+import "./styles.css";
 // fake data generator
 const getselectedItems = (count: number, offset = 0) =>
   Array.from({ length: count }, (v, k) => k).map((k) => ({
@@ -21,37 +21,39 @@ const ComunicationPage: React.FC = () => {
   return (
     <Page pageTitle="Comunicarse" showHomeButton>
       {/* <div style={{ display: "flex", width: "inherit" }}> */}
-        <ReactSortable
-          list={selectedItems}
-          setList={setselectedItems}
-          animation={150}
-          group="shared-group-name"
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(auto-fill, minmax(4rem, 1fr))",
-            gap: "0.5rem",
-          }}
-        >
-          {selectedItems.map((item) => (
-            <IonItem key={item.id}>{item.content}</IonItem>
-          ))}
-        </ReactSortable>
+      <ReactSortable
+        list={selectedItems}
+        setList={setselectedItems}
+        animation={150}
+        group="shared-group-name"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(4rem, 1fr))",
+          gap: "0.5rem",
+        }}
+      >
+        {selectedItems.map((item) => (
+          <IonItem key={item.id}>{item.content}</IonItem>
+        ))}
+      </ReactSortable>
 
-        <ReactSortable
-          list={availableItems}
-          setList={setAvailableItems}
-          animation={150}
-          group="shared-group-name"
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(auto-fill, minmax(4rem, 1fr))",
-            gap: "0.5rem",
-          }}
-        >
-          {availableItems.map((item) => (
-            <IonItem key={item.id}>{item.content}</IonItem>
-          ))}
-        </ReactSortable>
+      <ReactSortable
+        list={availableItems}
+        setList={setAvailableItems}
+        animation={150}
+        group="shared-group-name"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(4rem, 1fr))",
+          gap: "0.5rem",
+        }}
+      >
+        {availableItems.map((item) => (
+          <IonItem key={item.id} className="specialItem">
+            {item.content}
+          </IonItem>
+        ))}
+      </ReactSortable>
       {/* </div> */}
     </Page>
   );
