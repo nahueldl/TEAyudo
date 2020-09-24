81efda3c00f2f3d6ad7085e0d659e72258b1fdc1
LPapazian <lilapapazian@gmail.com>
2020-09-24 16:28:22 -0300

    add categories services

---
+++ src/services/categories.services.ts
@@ -0,0 +1,64 @@
+import { stringify } from "querystring";
+import AxiosWrapper from "../utils/axios";
+
+class CategoriesService {
+  private axios: AxiosWrapper;
+  constructor() {
+    this.axios = new AxiosWrapper({ useErrorInterceptor: true });
+  }
+
+  getCategories(token: string, patientId: string) {
+    const header = `Bearer ${token}`;
+
+    return this.axios.get("/api/categorias", {
+      headers: {
+        Authorization: header,
+      },
+      params: {
+        paciente: patientId,
+      },
+    });
+  }
+
+  createCategory(token: string, categoryName: string, idRol: string) {
+    const header = `Bearer ${token}`;
+    return this.axios.post("/api/categorias", {
+      headers: {
+        Authorization: header,
+      },
+      params: {
+        nombre: categoryName,
+        id_rol: idRol,
+      },
+    });
+  }
+
+  editCategory(token: string, categoryName: string) {
+    const header = `Bearer ${token}`;
+    return this.axios.put("/api/categorias", {
+      headers: {
+        Authorization: header,
+      },
+      params: {
+        nombre: categoryName,
+      },
+    });
+  }
+
+  deleteCategory(token: string, categoryId: string) {
+    const header = `Bearer ${token}`;
+    return this.axios.delete("/api/categorias", {
+      headers: {
+        Authorization: header,
+      },
+      params: {
+        id: categoryId,
+      },
+    });
+  }
+}
+
+const instance = new CategoriesService();
+Object.freeze(instance);
+
+export default instance;
