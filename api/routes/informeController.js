46f85e2e050c16d6dd93dfc4546a74fbf594a3ff
Nahuel De Leonardis <deleonardisnahuel@gmail.com>
2020-09-24 13:42:02 -0300

    Horrible pero funka

--- api/routes/informeController.js
+++ api/routes/informeController.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const informeService = require('../services/informeService');
 const isAuth = require('../middleware/auth').isAuth;
 const estadosRespuesta = require('../models/estados_respuesta');
-const { fstat } = require('fs');
 
 //GET Informe by id  COMENTO ESTE GET PARA TESTEAR LO DE ABAJO NOMAS, NO PORQUE ESTE MAL
 /*
@@ -24,15 +23,15 @@ router.get('/:id', async (req, res) => {
 //al terminar, volver a ponerlo como post y ponerle el isAuth
 //POST Informe
 router.get('/', async (req, res) => {
-	const result = await informeService.insert(req.body, req.user);
-	if(result.state === estadosRespuesta.OK){
-		res.setHeader('Content-Type', 'application/pdf');
-		res.status(200).send(result.response);
-	}else if(result.state === estadosRespuesta.SERVERERROR){
-		res.status(500).json({msg: "Ha ocurrido un error inesperado en el servidor"});
-	}else if(result.state === estadosRespuesta.USERERROR){
-		res.status(400).json({msg: result.response});
-	}
+	const result = await informeService.insert(req.body, req.user, res);
+	// if(result.state === estadosRespuesta.OK){
+	// 	//res.setHeader('Content-Type', 'application/pdf');
+	// 	//res.status(200).send(result.response);
+	// }else if(result.state === estadosRespuesta.SERVERERROR){
+	// 	res.status(500).json({msg: "Ha ocurrido un error inesperado en el servidor"});
+	// }else if(result.state === estadosRespuesta.USERERROR){
+	// 	res.status(400).json({msg: result.response});
+	// }
 });
 
 module.exports = router;
--- api/services/informeService.js
+++ api/services/informeService.js
@@ -2,46 +2,50 @@ const http = require('http');
 const informeDAO = require('../persistence/informeDAO');
 const estadosRespuesta = require('../models/estados_respuesta');
 const Handlebars = require("handlebars");
-
+const ts = require('stream').Transform();
+const pdf = require('html-pdf');
 
 const informeService = {
 
-    getById: async function(id_informe, usuario){
+	getById: async function(id_informe, usuario){
 		//Aca iría la lógia de negocio
 		return await informeDAO.getById(parseInt(id_informe), usuario.id_usuario);
 	},
 
-    insert: async function(categoria, usuario){
+	insert: async function(categoria, usuario, res){
 
-        //Utilizacion de handlebars para darle forma al PDF
-        let source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
-                    "{{kids.length}} kids:</p>" +
-                    "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
-        let template = Handlebars.compile(source);
-        
-        let data = { "name": "Alan", "hometown": "Somewhere, TX",
-                    "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
-        let resultado = template(data);
+		//Utilizacion de handlebars para darle forma al PDF
+		let source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
+					"{{kids.length}} kids:</p>" +
+					"<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+		let template = Handlebars.compile(source);
+		
+		let data = { "name": "Alan", "hometown": "Somewhere, TX",
+					"kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+		let resultado = template(data);
 
-       
-        //Renderizacion y creacion del PDF
-        let informe;
-        let fs = require('fs');
-        let pdf = require('html-pdf');
-        let html = resultado; //esto deberia tomar el result de arriba
-        let options = { format: 'Letter' };
-        
-        pdf.create(html).toStream(async function(err, stream) {
-            informe = stream;
-         });
+	   
+		//Renderizacion y creacion del PDF
+		let informe = null;
+		
+		const html = resultado; //esto deberia tomar el result de arriba
+		const options = { format: 'A4' };
+		
+		pdf.create(html, options).toStream((err, stream) => {
+			if(err === undefined || err === null){
+				res.setHeader('Content-Type', 'application/pdf');
+				res.status(200);
+				stream.pipe(res);
+				// res.end();
+			}
+		});
 
-        const result = {
-            state: estadosRespuesta.OK,
-            response: informe
-            }
-            return result;       
-        
-    }
+		//Si alguien ve este pieza de codigo me asesinan
+		// while(informe === null){
+		// 	setTimeout(() =>{},50);
+		// }
+
+	}
 
 }
 
