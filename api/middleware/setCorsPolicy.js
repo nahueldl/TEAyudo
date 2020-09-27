60772bf8fdf771d470e0392b73d93106bca43263
Nahuel De Leonardis <deleonardisnahuel@gmail.com>
2020-09-27 18:09:29 -0300

    Fix add Access-Control-Allow-Methods to CORS Policy

--- api/middleware/setCorsPolicy.js
+++ api/middleware/setCorsPolicy.js
@@ -1,5 +1,6 @@
 
 const corsPolicy = (req, res, next) => {
+	res.header("Access-Control-Allow-Methods", "*");
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "*");
 	next();
