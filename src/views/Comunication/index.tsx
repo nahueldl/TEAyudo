913652e9c28ade89f8b5a51769f4ba43b7b368fd
LPapazian <lilapapazian@gmail.com>
2020-09-29 20:07:52 -0300

    comunication now working

--- src/views/Comunication/index.tsx
+++ src/views/Comunication/index.tsx
@@ -7,6 +7,7 @@ import CardWithImage from "../../components/CardWithImage";
 import { PlatformContext } from "../../context/platform";
 import CategoriesServices from "../../services/categories.services";
 import { AuthenticationContext } from "../../context/authentication";
+import PictogramsServices from "../../services/pictograms.services";
 
 const ComunicationPage: React.FC = () => {
   const [selectedItems, setselectedItems] = useState<any[]>([]);
@@ -14,21 +15,29 @@ const ComunicationPage: React.FC = () => {
   const [categories, setCategories] = useState<any[]>();
 
   const { isMobile } = useContext(PlatformContext).platformData;
-  const { authData, setAuthData } = useContext(AuthenticationContext);
+  const { authData } = useContext(AuthenticationContext);
+  const { token, patientId } = authData;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => getCategories(), []);
 
   const getCategories = () => {
-    setAuthData({ loading: true, error: false });
-    CategoriesServices.getCategories(authData.token!, authData.patientId!)
+    CategoriesServices.getCategories(token!, patientId!)
       .then((res: any) => {
         setCategories(res.data);
-        console.log("categorias", res.data);
-        setAuthData({ loading: false });
       })
       .catch((_error: any) => {
-        setAuthData({ loading: false, error: true });
+        console.log(_error);
       });
+  };
+
+  const fetchPictograms = (categoryId: number) => {
+    console.log("fetch pictogramas de categoria", categoryId);
+    PictogramsServices.getPictogramsByCategory(token!, categoryId, patientId!)
+      .then((res: any) => {
+        console.log(res);
+        setAvailableItems(res.data);
+      })
+      .catch((error: any) => console.log(error));
   };
 
   return (
@@ -56,8 +65,13 @@ const ComunicationPage: React.FC = () => {
         <IonRow>
           <IonCol>
             <IonList>
-              {categories?.map((item) => (
-                <IonItem detail>{item.nombre}</IonItem>
+              {categories?.map((item, index) => (
+                <IonItem
+                  key={index}
+                  onClick={() => fetchPictograms(item.id_categoria)}
+                >
+                  {item.nombre}
+                </IonItem>
               ))}
             </IonList>
           </IonCol>
@@ -69,8 +83,9 @@ const ComunicationPage: React.FC = () => {
               group="shared-group-name"
               className={`sortable ${isMobile ? " mobile" : ""}`}
             >
-              {availableItems!.map((item) => (
+              {availableItems!.map((item, index) => (
                 <CardWithImage
+                  key={index}
                   img={{ src: item.ruta_acceso_local, alt: "" }}
                   touchable={false}
                   onClick={() => {}}
