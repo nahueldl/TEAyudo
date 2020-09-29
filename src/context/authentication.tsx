03af929ed01dc87f4bf52d85228ab326a8c3c4a9
LPapazian <lilapapazian@gmail.com>
2020-09-29 19:03:27 -0300

    fix typo

--- src/context/authentication.tsx
+++ src/context/authentication.tsx
@@ -21,7 +21,7 @@ const AuthenticationProvider = (props: any) => {
     Storage.get({ key: "patientName" }).then((res) =>
       setAuthData({ patientName: res.value! })
     );
-    Storage.get({ key: "patientId " }).then((res) =>
+    Storage.get({ key: "patientId" }).then((res) =>
       setAuthData({ patientId: res.value! })
     );
     Storage.get({ key: "rol" }).then((res) => {
