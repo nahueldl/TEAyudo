cee6ec8a2fa3a498c1c8a4bfdbe5832a5a760640
Juani Ferro <juanignacioferro8@gmail.com>
2020-10-14 22:51:31 -0300

    base for informe, still needs to use handlebars properly

--- api/services/informeService.js
+++ api/services/informeService.js
@@ -7,7 +7,7 @@ const pdf = require('html-pdf');
 //let inf = require('./informe.hbs');
 const fs = require('fs');
 const path = require('path');
-
+const Chart = require('chart.js');
 
 const informeService = {
 
@@ -26,7 +26,7 @@ const informeService = {
 
 		const ruta = path.join(__dirname, '../templates/informe.hbs');
 
-		const handlebars = fs.readFileSync(path.join(__dirname, '../templates/informe.hbs'));
+		const handlebars = fs.readFileSync(path.join(__dirname, '../templates/informe.hbs'), "utf8");
 		
 		let source2 = handlebars;
 
@@ -62,3 +62,52 @@ const informeService = {
 }
 
 module.exports = informeService;
+
+
+
+
+
+
+/*
+<!DOCTYPE html>
+<html lang="en"> 
+  
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content= 
+        "width=device-width, initial-scale=1.0"> 
+  
+    <title>Pie Chart</title> 
+  
+    <style> 
+        .piechart { 
+            margin-top: 300px; 
+            display: block; 
+            position: absolute; 
+            width: 400px; 
+            height: 400px; 
+            border-radius: 50%; 
+            background-image: conic-gradient( 
+                pink 70deg,  
+                lightblue 0 235deg,  
+                orange 0); 
+        } 
+  
+        body, 
+        .piechart { 
+            display: flex; 
+            justify-content: center; 
+            align-items: center; 
+        } 
+    </style> 
+</head> 
+  
+<body> 
+    <h1>Pie Chart</h1> 
+    <div class="piechart"></div> 
+    <script> src="informeService.js" </script>
+    <script id="informe" type="text/x-handlebars-template">
+</body> 
+  
+</html> 
+*/
