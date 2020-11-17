2774a4c5558daa1e02f13c96ae6193622c122115
Nahuel De Leonardis <deleonardisnahuel@gmail.com>
2020-11-17 14:17:29 -0300

    CORS Policy 3?

--- api/middleware/setCorsPolicy.js
+++ api/middleware/setCorsPolicy.js
@@ -2,7 +2,7 @@
 const corsPolicy = (req, res, next) => {
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization");
+	res.header("Access-Control-Allow-Headers", "*");
 	res.header("Access-Control-Allow-Credentials", "True");
 	next();
 }
