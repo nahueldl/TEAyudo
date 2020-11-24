af673613b98d34c204916c7ab1573dd9f646eb26
Tomi <tomasmazzocchi@est.frba.utn.edu.ar>
2020-11-24 11:23:47 -0300

    validate count patient from user to show or not change patient button

--- src/views/Configuration/index.tsx
+++ src/views/Configuration/index.tsx
@@ -1,19 +1,39 @@
 import Page from "../../components/Page";
-import React, { useCallback, useContext } from "react";
-import { IonButton, IonCol, IonGrid, IonRow, NavContext } from "@ionic/react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { IonButton, IonCol, IonGrid, IonLoading, IonRow, NavContext } from "@ionic/react";
 import "./styles.css";
 import { clear } from "../../services/storage.services";
 import { PatientContext } from "../../context/patient";
 import { AuthenticationContext } from "../../context/authentication";
 import { CategoryContext } from "../../context/category";
 import { RegistrationContext } from "../../context/registration";
+import PatientServices from "../../services/patients.services";
 
 const ConfigurationPage: React.FC = () => {
   const { navigate } = useContext(NavContext);
-  const { setAuthData } = useContext(AuthenticationContext);
+  const { authData, setAuthData } = useContext(AuthenticationContext);
   const { setPatientData } = useContext(PatientContext);
   const { setCategoriaData } = useContext(CategoryContext);
   const { setRegistrationData } = useContext(RegistrationContext);
+  const [loading, isLoading] = useState<boolean>(true);
+  const [showChangePatient, setShowChangePatient] = useState<boolean>(false);
+
+  useEffect(() => { getPatients(); }, []);
+
+  const getPatients = () => {
+    isLoading(true);
+    PatientServices.getPatientsFromUser(authData.token!)
+      .then((res: any) => {
+        const length = res.data.length;
+        if (length > 1) {
+          setShowChangePatient(true);
+        }
+        isLoading(false);
+      })
+      .catch((error: any) => {
+        isLoading(false);
+      });
+  };
 
   const goToLoginPage = useCallback(() => navigate("/login"), [navigate]);
 
@@ -37,14 +57,21 @@ const ConfigurationPage: React.FC = () => {
       <IonGrid className="overflow-auto">
         <IonRow>
           <IonCol size="5" sizeMd="4" className="margin-auto">
-            <div className="mt-5">
-              <IonButton onClick={() => changePatient()} expand="block">Cambiar paciente</IonButton>
-            </div>
+            {showChangePatient? (
+              <div className="mt-5">
+                <IonButton onClick={() => changePatient()} expand="block">Cambiar paciente</IonButton>
+              </div>
+            ):null}
             <div className="mt-5">
               <IonButton onClick={() => logout()} color="danger" expand="block">Cerrar sesión</IonButton>
             </div>
           </IonCol>
         </IonRow>
+        <IonLoading
+            isOpen={loading}
+            message={"Trabajando..."}
+            spinner="crescent"
+        />
       </IonGrid>
     </Page>
   );
